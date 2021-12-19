9e84ada19cc282553b8c684ad05ef6eda94da8f4
Marc Ablay <marc.ablay@gmail.com>
2021-12-19 23:25:35 +0100

    added spec

---
+++ test/events.spec.js
@@ -0,0 +1,37 @@
+import { test } from 'zora'
+import { EventEmitter } from '../index.js'
+
+test('smoketest', t => {
+  const ee = new EventEmitter()
+  let foo = 0
+  const fn = () => foo++
+
+  ee.on('foo', fn)
+  t.equal(ee.listenerCount('foo'), 1, 'count listeners')
+  t.equal(ee.eventNames(), ['foo'], 'list event names')
+
+  t.equal(foo, 0, 'do not execute listener on register')
+  ee.emit('foo')
+  t.equal(foo, 1, 'execute listener on emit')
+  ee.emit('foo')
+  t.equal(foo, 2, 'execute listener on second emit')
+
+  ee.off('foo', fn)
+  t.equal(ee.listenerCount('foo'), 0, 'do not count removed listeners')
+  ee.emit('foo')
+  t.equal(foo, 2, 'do not execute listener after it has been removed')
+
+  t.equal(ee.listenerCount('bar'), 0, 'do not count bougus listeners')
+})
+
+test('once', t => {
+  const ee = new EventEmitter()
+  let foo = 0
+  ee.once('foo', () => foo++)
+  t.equal(foo, 0, 'do not execute once-listener on register')
+  ee.emit('foo')
+  t.equal(foo, 1, 'execute once-listener on first emit')
+  ee.emit('foo')
+  t.equal(foo, 1, 'do not execute once-listener on second emit')
+  t.equal(ee.eventNames().length, 0, 'once-listener gets removed after use')
+})
