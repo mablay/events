ac83c55fbea1cf0cd5d84aa66a28a6e13418f708
Marc Ablay <marc.ablay@gmail.com>
2021-12-19 21:50:16 +0100

    WIP updating release script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+v="${1:-patch}"
+# rm -rf node_modules
+# npm install
+npm run build
+npm test
+tag=`npm version $v`
+echo "publishing $tag"
+# git push && git push --tags
+# npm publish --tag $tag
